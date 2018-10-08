bb721c3a4883cca1385af9d0872c6edc00f3d37f
Luciano Sclovsky <luciano.sclovsky@SKY.local>
2018-10-08 19:54:05 -0300

    Created nestedCell

--- CollectionInTable/NestedTableViewController.swift
+++ CollectionInTable/NestedTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NestedTableViewController: UITableViewController {
+class NestedTableViewController: UITableViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +22,49 @@ class NestedTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return 20
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
-        cell.textLabel?.text = "basicCell \(indexPath.row)"
-        return cell
+        if indexPath.row == 3 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "nestedCell", for: indexPath)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+            cell.textLabel?.text = "basicCell \(indexPath.row)"
+            return cell
+        }
     }
 
+     // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row == 3 ? 100 : 60
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        if let label = cell.viewWithTag(1) as? UILabel {
+            label.text = "\(indexPath.row)"
+        }
+        return cell
+    }
+    
+    // UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
 }
